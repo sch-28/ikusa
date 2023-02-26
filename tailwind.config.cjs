37d3f0ed6827bf60b85f18614d0a31f00860919c
sch-28 <jansch2803@googlemail.com>
2023-02-26 21:23:32 +0100

    chore: added flowbite, toast, icons, dayjs, vlist, modal

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
 	theme: {
 		extend: {
 			colors: {
@@ -131,5 +134,6 @@ module.exports = {
 			}
 		}
 	},
-	plugins: []
+	plugins: [require('flowbite/plugin')],
+	darkMode: 'class'
 };
