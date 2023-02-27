548962ef4fd893b3f758e1b522360eae345c3e8c
sch-28 <jansch2803@googlemail.com>
2023-02-27 02:09:50 +0100

    feat: war overview

--- src/routes/wars/+page.svelte
+++ src/routes/wars/+page.svelte
@@ -1,18 +1,30 @@
 <script>
 	import Checkbox from '../../components/elements/checkbox.svelte';
+	import Icon from '../../components/elements/icon.svelte';
 	import { Manager } from '../../logic/manager';
+	import GiSkullCrack from 'svelte-icons/gi/GiSkullCrack.svelte';
+	import GiCrownedSkull from 'svelte-icons/gi/GiCrownedSkull.svelte';
 </script>
 
-{#each $Manager.wars as war}
-	<div class="flex flex-col gap-2">
-		<div class="flex gap-2">
-			<div class="w-5 h-5">
-				<Checkbox />
-			</div>
+<div class="flex justify-between mb-4">
+	<p>All Wars</p>
+	<p>20/30</p>
+</div>
+
+<div class="flex flex-col">
+	{#each $Manager.wars as war}
+		<div class="flex gap-2 hover:bg-gray-700 rounded-lg p-2 cursor-pointer">
+			<!-- <div class="w-5 h-5">
+<Checkbox />
+</div> -->
+			<Icon
+				icon={war.won ? GiCrownedSkull : GiSkullCrack}
+				class="{war.won ? 'text-submarine-500' : 'text-red-500'} self-center"
+			/>
 			<div class="flex flex-col gap-1">
 				<div class="text-sm font-medium">{war.name}</div>
 				<div class="text-xs text-gray-500">{war.date}</div>
 			</div>
 		</div>
-	</div>
-{/each}
+	{/each}
+</div>
