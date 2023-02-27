6a544f6580382b90100e92d33cf353f8742fd300
sch-28 <jansch2803@googlemail.com>
2023-02-27 17:58:37 +0100

    feat: auto-complete element

--- src/components/elements/auto-complete.svelte
+++ src/components/elements/auto-complete.svelte
@@ -1,13 +1,92 @@
 <script lang="ts">
-	import { Input } from 'flowbite-svelte';
-
-	export let suggestions: string[] = [];
+	import { onDestroy, onMount } from 'svelte';
+	import Input from './input.svelte';
+	export let items: string[] = [];
 	export let value = '';
+	export let placeholder = '';
+	export let required = false;
+
+	let selected_suggestion = -1;
+	let suggestions: { name: string; button?: HTMLButtonElement }[] = [];
+
+	let input: HTMLInputElement;
+	let input_width: string = 'auto';
+	$: {
+		if (input) {
+			input_width = input.clientWidth.toString() + 'px';
+		}
+	}
+
+	$: items && value && filter_suggestions();
+
+	function filter_suggestions() {
+		suggestions = items
+			.filter(
+				(item) =>
+					item.toLowerCase().includes(value.toLowerCase()) && value.length > 0 && value !== item
+			)
+			.splice(0, 3)
+			.map((item) => ({ name: item, button: undefined }));
+
+		selected_suggestion = -1;
+	}
+
+	function handle_keydown(event: KeyboardEvent) {
+		if (event.key === 'Escape') {
+			suggestions = [];
+		} else if (event.key === 'ArrowDown') {
+			if (suggestions.length > 0) {
+				select_suggestion(1);
+			}
+		} else if (event.key === 'ArrowUp') {
+			if (suggestions.length > 0) {
+				select_suggestion(-1);
+			}
+		}
+	}
+
+	function select_suggestion(dir: -1 | 1) {
+		if (suggestions.length > 0) {
+			if (selected_suggestion + dir >= -2) {
+				selected_suggestion += dir;
+			}
+
+			if (selected_suggestion >= suggestions.length) {
+				selected_suggestion = -1;
+			} else if (selected_suggestion === -2) {
+				selected_suggestion = suggestions.length - 1;
+			}
+
+			if (selected_suggestion === -1) {
+				input.focus();
+				setTimeout(() => {
+					input.setSelectionRange(value.length, value.length);
+				}, 0);
+			} else {
+				suggestions[selected_suggestion].button?.focus();
+			}
+		}
+	}
+
+	onMount(() => {
+		document.addEventListener('keydown', handle_keydown);
+	});
+	onDestroy(() => {
+		document.removeEventListener('keydown', handle_keydown);
+	});
 </script>
 
-<Input bind:value on:change  />
-<div>
+<Input bind:value on:change {required} {placeholder} bind:input />
+<div
+	class="border border-gray-600 mt-1 absolute bg-gray-700 z-50 rounded-lg overflow-hidden {suggestions.length === 0 &&
+		'hidden'}"
+	style="width: {input_width};"
+>
 	{#each suggestions as suggestion}
-		<div>{suggestion}</div>
+		<button
+			class="p-2 w-full text-left text-white hover:bg-gold hover:text-black outline-none focus:bg-gold focus:text-black"
+			bind:this={suggestion.button}
+			on:click={() => (value = suggestion.name)}>{suggestion.name}</button
+		>
 	{/each}
 </div>
---
+++ src/components/elements/input.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	interface $$Props {
+		class?: string;
+		id?: string;
+		required?: boolean;
+		type?: string;
+		value?: string | number;
+		placeholder?: string;
+        input?: HTMLInputElement;
+	}
+	export let value: string | number = '';
+	export let type: string = 'text';
+    export let input: HTMLInputElement|undefined = undefined;
+
+	const handleInput = (e: any) => {
+		value = type.match(/^(number|range)$/) ? +e.target.value : e.target.value;
+	};
+</script>
+
+<input
+    bind:this={input}
+	{value}
+	{type}
+	placeholder={$$props.placeholder || ''}
+	required={$$props.required || false}
+	id={$$props.id}
+	on:input={handleInput}
+	class="outline-none block w-full disabled:cursor-not-allowed disabled:opacity-50 border focus:!border-blue-500 focus:!ring-blue-500 bg-gray-700 text-white placeholder-gray-400 border-gray-600 p-2.5 text-sm rounded-lg {$$props.class}"
+/>
