8a23083e30c2cb488434d40262c13d77e92fa1be
sch-28 <jansch2803@googlemail.com>
2023-03-02 00:29:25 +0100

    feat: icon sizes

--- src/components/elements/icon.svelte
+++ src/components/elements/icon.svelte
@@ -4,11 +4,18 @@
 	interface $$Props {
 		class?: string;
 		icon: Component;
+		size?: 'sm' | 'md' | 'lg';
 	}
 </script>
 
 {#if icon}
-	<div class="w-5 h-5 {$$props.class}">
+	<div
+		class="{$$props.size === 'sm'
+			? 'w-4 h-4'
+			: $$props.size === 'lg'
+			? 'w-8 h-8'
+			: 'w-5 h-5'} {$$props.class}"
+	>
 		<svelte:component this={icon} />
 	</div>
 {/if}
