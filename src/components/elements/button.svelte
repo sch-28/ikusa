ca388ab61262cd30056deaa10f700dc941d68bb1
sch-28 <jansch2803@googlemail.com>
2023-03-02 01:06:08 +0100

    feat: button disabled color

--- src/components/elements/button.svelte
+++ src/components/elements/button.svelte
@@ -13,7 +13,10 @@
 	disabled={$$props.disabled || false}
 	id={$$props.id}
 	btnClass="
-	{$$props.color === 'primary' || (!$$props.color && 'bg-gold-300 focus:ring-gold-400 text-black')}
+	{($$props.color === 'primary' || !$$props.color) &&
+		`bg-gold-300 focus:ring-gold-400 text-black ${
+			$$props.disabled ? '!bg-gray-700 text-gray-400' : 'border-gold'
+		}`}
 	{$$props.color === 'secondary' && 'bg-black  focus:ring-gray-400 border border-gold'}
 	text-center font-medium focus:ring-4 focus:outline-none flex items-center justify-center px-5 py-2.5 text-sm rounded-lg {$$props.class} h-10"
 >
