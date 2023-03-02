8d30ea55152f977947843893e833274f755ffb19
sch-28 <jansch2803@googlemail.com>
2023-03-02 01:16:06 +0100

    refacor: wars hover color

--- src/routes/wars/+page.svelte
+++ src/routes/wars/+page.svelte
@@ -10,26 +10,33 @@
 
 <div class="flex justify-between mb-4">
 	<p>All Wars</p>
-	<p>20/30</p>
+	<p>{$Manager.wars.length} Wars</p>
 </div>
 
 <div class="flex flex-col">
 	{#each $Manager.wars as war}
-		<a class="flex gap-2 hover:bg-gray-700 rounded-lg p-2 cursor-pointer" href="/wars/{war.id}">
-			<!-- <div class="w-5 h-5">
-<Checkbox />
-</div> -->
+		<a
+			class="flex gap-2 hover:text-black text-gold rounded-lg p-2 cursor-pointer hover:bg-gold group"
+			href="/wars/{war.id}"
+		>
 			<Icon
 				icon={war.won ? GiCrownedSkull : GiSkullCrack}
-				class="{war.won ? 'text-submarine-500' : 'text-red-500'} self-center"
+				class="{war.won
+					? 'text-submarine-500 group-hover:text-submarine-700'
+					: 'text-red-500 group-hover:text-red-800'} self-center"
 			/>
 			<div class="flex flex-col gap-1">
 				<div class="text-sm font-medium">{war.name}</div>
-				<div class="text-xs text-gray-500">{war.date}</div>
+				<div class="text-xs text-gray-500 group-hover:text-gray-900">{war.date}</div>
 			</div>
 
-			<button on:click={() => ModalManager.open(WarForm, { war: war })} class="ml-auto"
-				><Icon icon={MdSettings} class="self-center " /></button
+			<button
+				on:click={(e) => {
+					e.stopPropagation();
+					e.preventDefault();
+					ModalManager.open(WarForm, { war: war });
+				}}
+				class="ml-auto"><Icon icon={MdSettings} class="self-center " /></button
 			>
 		</a>
 	{/each}
