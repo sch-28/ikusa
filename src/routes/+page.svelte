ac3df5a9191ae1edbfa456f948a86b3977611682
sch-28 <jansch2803@googlemail.com>
2023-03-25 23:36:11 +0100

    feat: landing page improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
-	import Button from "../components/elements/button.svelte";
-
+	import { goto } from '$app/navigation';
+	import Button from '../components/elements/button.svelte';
 </script>
-<div class="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
+
+<div
+	class="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 flex items-center justify-center flex-col"
+>
 	<h1 class="text-5xl font-bold">Ikusa</h1>
-	<p>a BDO combat analyzer</p>
-	<Button on:click={() => goto("/dashboard")}>Dashboard</Button>
+	<p class="text-center text-lg max-w-lg mt-2">
+		Ikusa is a powerful tool that allows you to analyze your game logs and gain valuable insights
+		into your combat performance.
+	</p>
+	<div class="mt-4 flex gap-2">
+		<Button on:click={() => goto('/dashboard')}>Dashboard</Button>
+		<Button color="secondary" on:click={() => goto('/dashboard')}>Get Started</Button>
+	</div>
 </div>
