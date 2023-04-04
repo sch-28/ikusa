1132f107cd7d063327a9f25cf51782eae23c5fe3
sch-28 <jansch2803@googlemail.com>
2023-04-04 23:59:57 +0200

    feat: documentation sidebar toggle

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import IoIosMenu from 'svelte-icons/io/IoIosMenu.svelte';
+	import IoIosClose from 'svelte-icons/io/IoIosClose.svelte';
+	import Icon from '../../components/elements/icon.svelte';
 
 	type Item = {
 		title: string;
@@ -21,12 +24,12 @@
 		{ title: 'Combat Logger' },
 		{
 			title: 'Installation',
-			description: 'Learn how to install the combat logger',
+			// description: 'Learn how to install the combat logger',
 			link: '/docs/installation'
 		},
 		{
 			title: 'Configuration & Usage',
-			description: 'Learn how to configure and use the combat logger',
+			// description: 'Learn how to configure and use the combat logger',
 			link: '/docs/usage'
 		},
 
@@ -37,22 +40,38 @@
 			link: '/docs/adding-a-new-war'
 		}
 	];
-    
+	let visible = false;
 </script>
 
-<div class="flex flex-col gap-1">
+<button class="sm:hidden block" on:click={() => (visible = !visible)}>
+	<Icon icon={IoIosMenu} />
+</button>
+<div
+	class="flex flex-col gap-1 sm:relative absolute transition top-0 bottom-0 left-0 bg-black border-gold border rounded-lg {visible
+		? 'translate-x-4'
+		: '-translate-x-full'} "
+>
+	<div class="sm:hidden block mb-52">
+		<button on:click={() => (visible = !visible)}>
+			<Icon icon={IoIosClose} />
+		</button>
+	</div>
 	{#each content as entry}
 		{#if 'link' in entry}
 			<a href={entry.link}>
 				<p
-					class="font-light {$page.params.entry === entry.link.split("/")[2] ? 'text-gold' : 'text-gold-muted'}"
+					class="font-light {$page.params.entry === entry.link.split('/')[2]
+						? 'text-gold'
+						: 'text-gold-muted'}"
 					title={entry.description}
 				>
 					{entry.title}
 				</p>
 			</a>
 		{:else}
-			<p class="font-bold text-gold-muted uppercase tracking-wider [&:not(:first-child)]:mt-4">{entry.title}</p>
+			<p class="font-bold text-gold-muted uppercase tracking-wider [&:not(:first-child)]:mt-4">
+				{entry.title}
+			</p>
 		{/if}
 	{/each}
 </div>
