c677111581d0e60e9820193acb421cf87f4293b0
sch-28 <jansch2803@googlemail.com>
2023-04-07 02:54:34 +0200

    feat: added login button

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import Icon from './elements/icon.svelte';
+	import IoMdHelp from 'svelte-icons/io/IoMdHelp.svelte';
 
 	$: is_selected = (url: string) => {
 		return $page.route.id?.includes(url);
@@ -10,27 +12,25 @@
 <header class="flex items-center justify-between py-4 ">
 	<div class="flex gap-2 items-end">
 		<a class="text-3xl font-bold mr-2" href="/">Ikusa</a>
-		<a
-			href="/wars"
-			class={is_selected('wars') ? 'text-gold' : 'text-gold-muted hover:text-gold'}>Wars</a
+		<a href="/wars" class={is_selected('wars') ? 'text-gold' : 'text-gold-muted hover:text-gold'}
+			>Wars</a
 		>
 		<a
 			href="/players"
-			class={is_selected('players') ? 'text-gold' : 'text-gold-muted hover:text-gold'}
-			>Players</a
+			class={is_selected('players') ? 'text-gold' : 'text-gold-muted hover:text-gold'}>Players</a
 		>
 		<a
 			href="/guilds"
-			class={is_selected('guilds') ? 'text-gold' : 'text-gold-muted hover:text-gold'}
-			>Guilds</a
+			class={is_selected('guilds') ? 'text-gold' : 'text-gold-muted hover:text-gold'}>Guilds</a
 		>
 	</div>
-	<div class="flex gap-4 items-end h-[2.25rem]">
+	<div class="flex gap-3 items-end h-[2.25rem]">
 		<a
 			href="/docs"
-			class={is_selected('docs') ? 'text-gold' : 'text-gold-muted hover:text-gold'}
-			>Docs</a
+			class="{is_selected('docs')
+				? 'text-gold'
+				: 'text-gold-muted hover:text-gold'} self-center mt-2"><Icon icon={IoMdHelp} /></a
 		>
-		<!-- <a href="/login" class="bg-gold text-black rounded-lg px-2 py-1 font-bold">Log in</a> -->
+		<a href="/discord/auth" class="bg-gold text-black rounded-lg px-2 py-1 font-bold">Log in</a>
 	</div>
 </header>
