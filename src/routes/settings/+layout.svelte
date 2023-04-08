e2ac0e3da0ead5f922ab272740f55b586fc1d8d5
sch-28 <jansch2803@googlemail.com>
2023-04-08 20:10:32 +0200

    feat: settings

---
+++ src/routes/settings/+layout.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import IoIosMenu from 'svelte-icons/io/IoIosMenu.svelte';
+	import IoIosClose from 'svelte-icons/io/IoIosClose.svelte';
+	import Icon from '../../components/elements/icon.svelte';
+
+	type Item = {
+		title: string;
+		description: string;
+		link: string;
+	};
+	type Section = {
+		title: string;
+	};
+
+	$: page_entry = $page.route.id;
+
+	const content: (Item | Section)[] = [
+		{ title: 'Settings' },
+		{ title: 'User', description: 'User settings', link: '/settings/user' },
+		{ title: 'Dashboard', description: 'Dashboard settings', link: '/settings/dashboard' }
+	];
+	let visible = false;
+</script>
+
+<div class="flex gap-2">
+	<div class="mr-8 sm:mr-0 flex">
+		<button class="sm:hidden absolute place-self-center" on:click={() => (visible = !visible)}>
+			<Icon icon={IoIosMenu} />
+		</button>
+		<div
+			class="sm:w-80 p-2 sm:p-0 sm:relative absolute transition left-0 bg-black border-gold border sm:border-0 sm:border-r rounded-lg sm:rounded-none {visible
+				? 'translate-x-4 sm:translate-x-0'
+				: '-translate-x-[105%] sm:translate-x-0'} "
+		>
+			<div class="sm:hidden flex">
+				<button on:click={() => (visible = !visible)}>
+					<Icon icon={IoIosClose} />
+				</button>
+			</div>
+			<div class="flex flex-col gap-1 ">
+				{#each content as entry}
+					{#if 'link' in entry}
+						<a href={entry.link}>
+							<p
+								class="font-light {page_entry === entry.link
+									? 'text-gold'
+									: 'text-gold-muted'}"
+								title={entry.description}
+							>
+								{entry.title}
+							</p>
+						</a>
+					{:else}
+						<p
+							class="font-bold text-sm text-gold-muted uppercase tracking-wider [&:not(:first-child)]:mt-10"
+						>
+							{entry.title}
+						</p>
+					{/if}
+				{/each}
+			</div>
+		</div>
+	</div>
+	<slot />
+</div>
---
+++ src/routes/settings/user/+page.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import { Label } from 'flowbite-svelte';
+	import { onMount } from 'svelte';
+	import AutoComplete from '../../../components/elements/auto-complete.svelte';
+	import { Manager } from '../../../logic/stores';
+	import { User } from '../../../logic/user';
+
+	let form_error = '';
+
+	let family_name = $User.name ?? '';
+	let guild_name = $User.guild ?? '';
+
+	onMount(() => {
+		family_name = $User.name ?? '';
+		guild_name = $User.guild ?? '';
+	});
+
+	$: {
+		family_name;
+		guild_name;
+		validate();
+	}
+
+	function validate() {
+		if (family_name.length === 0) {
+			form_error = 'Family name is required';
+			return false;
+		}
+		if (guild_name.length === 0) {
+			form_error = 'Guild name is required';
+			return false;
+		}
+
+		if (!$Manager.players.find((p) => p.name === family_name)) {
+			form_error = 'Family name is not valid';
+			return false;
+		}
+
+		if (!$Manager.guilds.find((g) => g.name === guild_name)) {
+			form_error = 'Guild name is not valid';
+			return false;
+		}
+
+		$User.name = family_name;
+		$User.guild = guild_name;
+		fetch('/api/user', {
+			method: 'POST',
+			body: JSON.stringify($User)
+		});
+		form_error = '';
+	}
+</script>
+
+<div>
+	<p class="capitalize font-semibold text-lg">User Settings</p>
+	<div class="text-gold-muted mb-2">
+		Setting your family name and guild here will allow you to get a overview of your current
+		performance in the <a href="/dashboard" class="underline">Dashboard</a>.
+	</div>
+	<div class="flex gap-2 mb-2">
+		<div>
+			<Label for="fam-name">Family Name</Label>
+			<AutoComplete
+				id="fam-name"
+				items={$Manager.players.map((p) => p.name)}
+				bind:value={family_name}
+			/>
+		</div>
+
+		<div>
+			<Label for="fam-name">Guild</Label>
+			<AutoComplete
+				id="fam-name"
+				items={$Manager.guilds.map((g) => g.name)}
+				bind:value={guild_name}
+			/>
+		</div>
+	</div>
+	<p class="text-red-500">{form_error}</p>
+</div>
