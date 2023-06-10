0412e77f6b5bd1a0b45440ca9e321e3e16476a34
sch-28 <jansch2803@googlemail.com>
2023-06-10 23:23:39 +0200

    feat: footer discord link

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,10 +1,14 @@
 <script>
 	import Icon from './elements/icon.svelte';
 	import GoMarkGithub from 'svelte-icons/go/GoMarkGithub.svelte';
+	import FaDiscord from 'svelte-icons/fa/FaDiscord.svelte'
 </script>
 
 <div class="w-full flex justify-between mt-auto mb-4">
 	<p>Made by <b>ORACLE#7672</b></p>
 
-	<a href="https://github.com/sch-28/ikusa"><Icon icon={GoMarkGithub} /></a>
+	<div class="flex gap-2">
+		<a href="https://discord.gg/nXSYGnxXJ5" title="Join the Discord!"><Icon icon={FaDiscord} /></a>
+		<a href="https://github.com/sch-28/ikusa_logger" title="GitHub Repository for the Logger"><Icon icon={GoMarkGithub} /></a>
+	</div>
 </div>
