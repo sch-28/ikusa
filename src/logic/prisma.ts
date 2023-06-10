02166e888bfcadf7e6b094acbd49945580b7a111
sch-28 <jansch2803@googlemail.com>
2023-06-10 15:16:19 +0200

    fix: prisma import

--- src/logic/prisma.ts
+++ src/logic/prisma.ts
@@ -1,12 +1,2 @@
-import type { PrismaClient as ImportedPrismaClient } from '@prisma/client';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url ?? __filename);
-
-const { PrismaClient: RequiredPrismaClient } = require('@prisma/client');
-
-const _PrismaClient: typeof ImportedPrismaClient = RequiredPrismaClient;
-
-class PrismaClient extends _PrismaClient {}
-
+import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient();
