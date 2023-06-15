bbdb69498345e92fa6667c099b1e97f724e07ca9
sch-28 <jansch2803@googlemail.com>
2023-06-16 00:01:57 +0200

    feat: debounce util

--- src/logic/util.ts
+++ src/logic/util.ts
@@ -66,3 +66,13 @@ export function get_remaining_height(el: HTMLElement, margin = 0) {
 export function generate_id() {
 	return generateUniqueId() as string;
 }
+
+export function debounce<T>(func: (...args: T[]) => void, timeout = 300) {
+	let timer: NodeJS.Timeout;
+	return (...args: T[]) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			func(...args);
+		}, timeout);
+	};
+}
