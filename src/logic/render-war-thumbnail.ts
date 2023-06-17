cb6a4501fc4490f0b01ba057b305f358ff2219ae
sch-28 <jansch2803@googlemail.com>
2023-06-17 06:10:02 +0200

    fix: catching puppeteer error

--- src/logic/render-war-thumbnail.ts
+++ src/logic/render-war-thumbnail.ts
@@ -4,34 +4,36 @@ import { supabase } from './supabase';
 import { CHROME_URL } from '$env/static/private';
 
 export async function render_preview(url: string, id: string) {
-	const browser = await puppeteer.launch({
-		args: chromium.args,
-		defaultViewport: chromium.defaultViewport,
-		executablePath: await chromium.executablePath(CHROME_URL),
-		headless: chromium.headless,
-		ignoreHTTPSErrors: true
-	});
-	const page = await browser.newPage();
-	await page.setViewport({
-		width: 1200,
-		height: 628
-	});
-	await page.goto(url, { waitUntil: 'networkidle0' });
-	const war_container = await page.$('.mt-16');
-	if (!war_container) return;
-	const buffer = await war_container.screenshot({ type: 'png' });
-	await browser.close();
-
-	const { data, error } = await supabase.storage
-		.from('war-thumbnails')
-		.upload(`${id}.png`, buffer, {
-			cacheControl: '3600',
-			upsert: true
+	try {
+		const browser = await puppeteer.launch({
+			args: chromium.args,
+			defaultViewport: chromium.defaultViewport,
+			executablePath: await chromium.executablePath(CHROME_URL),
+			headless: chromium.headless,
+			ignoreHTTPSErrors: true
 		});
+		const page = await browser.newPage();
+		await page.setViewport({
+			width: 1200,
+			height: 628
+		});
+		await page.goto(url, { waitUntil: 'networkidle0' });
+		const war_container = await page.$('.mt-16');
+		if (!war_container) return;
+		const buffer = await war_container.screenshot({ type: 'png' });
+		await browser.close();
 
-	if (error) {
-		console.error(error);
+		const { data, error } = await supabase.storage
+			.from('war-thumbnails')
+			.upload(`${id}.png`, buffer, {
+				cacheControl: '3600',
+				upsert: true
+			});
+
+		if (error) {
+			console.error(error);
+		}
+	} catch (e) {
+		console.error(e);
 	}
-
-	return data;
 }
