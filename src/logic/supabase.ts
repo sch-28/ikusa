76b17b39557d35a9d6540aa7c99205310a7796d5
sch-28 <jansch2803@googlemail.com>
2023-06-23 00:53:25 +0200

    fix: supabase warning

--- src/logic/supabase.ts
+++ src/logic/supabase.ts
@@ -1,3 +1,5 @@
 import { SUPABASE_KEY, SUPABASE_URL } from '$env/static/private';
 import { createClient } from '@supabase/supabase-js';
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+	auth: { persistSession: false }
+});
