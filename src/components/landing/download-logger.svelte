6a066764b79eeb6a678951cb866483b04d2c3f9d
sch-28 <jansch2803@googlemail.com>
2023-07-05 00:04:23 +0200

    fix: adjusted download url

--- src/components/landing/download-logger.svelte
+++ src/components/landing/download-logger.svelte
@@ -11,7 +11,7 @@
 <Button
 	class="flex gap-2 items-center justify-center leading-none {$$props.class}"
 	size="lg"
-	href="https://github.com/sch-28/ikusa_logger/releases/download/1.2.3/ikusa-logger-installer.exe"
+	href="https://github.com/sch-28/ikusa_logger/releases/download/1.2.7/ikusa-logger-installer.exe"
 >
 	<Icon icon={MdFileDownload} size="lg" />
 	Ikusa Logger
