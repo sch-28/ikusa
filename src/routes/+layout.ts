4f49bd63cfb9d8ad7fa29b32e50c23cfd95cbbc3
sch-28 <jansch2803@googlemail.com>
2023-07-11 00:07:57 +0200

    fix: disable posthog in dev

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,10 @@
 import posthog from 'posthog-js';
 import { browser } from '$app/environment';
 import { PUBLIC_POSTHOG_KEY, PUBLIC_POSTHOG_URL } from '$env/static/public';
+import { dev } from '$app/environment';
 
 export const load = async () => {
-	if (browser) {
+	if (browser && !dev) {
 		posthog.init(PUBLIC_POSTHOG_KEY, { api_host: PUBLIC_POSTHOG_URL });
 	}
 	return;
